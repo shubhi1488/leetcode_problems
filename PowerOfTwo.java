231e033335046691b40010b074c8e8ffe777f962
Shubhi Agarwal <shubhi1488@gmail.com>
2022-09-06 09:40:14 +0530

    Update PowerOfTwo.java

--- PowerOfTwo.java
+++ PowerOfTwo.java
@@ -21,3 +21,11 @@ class Solution {
     
 }
 }
+//Turn-off the light method-2
+class Solution {
+    public boolean isPowerOfTwo(int n){
+        if(n==0) return false;
+        long x=(long)n;
+        return (x & (x-1))==0;
+}
+}
