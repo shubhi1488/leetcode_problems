f3c4830e2b8dce21b1588576ef6f92ef218cd8c5
Shubhi Agarwal <shubhi1488@gmail.com>
2022-10-13 08:44:26 +0530

    Update BinaryTreeInorderTraversal.java

--- BinaryTreeInorderTraversal.java
+++ BinaryTreeInorderTraversal.java
@@ -26,3 +26,21 @@ class Solution {
         }
     
 }
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+ class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+      List<Integer>res=new ArrayList<Integer>();
+        Stack<TreeNode>stack=new Stack<TreeNode>();
+       TreeNode curr=root;
+        while(curr!=null || !stack.isEmpty()){
+            while(curr!=null){
+                stack.push(curr);
+                curr=curr.left;
+            }
+            curr=stack.pop();
+            res.add(curr.val);
+            curr=curr.right;
+        }
+        return res;
+}
+}
